1ab33906485f8acb5ec36f530f9937ead0231e08
ラッ・キー <lakky@ratsukinoMacBook-Pro.local>
2022-10-25 18:46:01 +0900

    chenge post

--- post/post.py
+++ post/post.py
@@ -6,7 +6,12 @@ headers = {
 }
 
 json_data = {
-    'voice': 'こんにちは',
+    'action': 'voice',
+    'content': 'hello from mac'
 }
+text  = 'hello from mac'
+json_resp = None
 
-response = requests.post('http://18.183.196.94:8080/json', headers=headers, json=json_data)
+response = requests.post('http://18.183.196.94:50021/audio_query?speaker=1', params ={"text": text, "speaker": 1} )
+
+print(response)
