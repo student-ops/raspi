71d4e006e704e586fff4029f423b9607556855dc
student-ops <gogakuec@gmail.com>
2022-10-28 11:05:48 +0900

    complete

--- post/post.py
+++ post/post.py
@@ -1,20 +1,22 @@
 import requests
+# from playsound import playsound
 import json
 
 json_data = {
     "action": "echo"
 }
 
-# url ='http://18.183.196.94:50021/handle'
+# url ='http://18.183.196.94:8081/handle'
 header = {'Content-Type':'application/json'}
-url ='http://localhost:8081/handle'
+url ='http://18.183.196.94:8081'
+# url ='http://localhost:8081'
 d = json.dumps(json_data)
-response = requests.post(url,headers = header,data=d)
+response = requests.post(url,headers = header,data = d)
+# response = requests.post(url,headers = header)
 print("respose status "+str(response.status_code))
-f = open("audio2.wav","wb")
+f = open("audio3.wav","wb")
 f.write(response.content)
 f.close()
-# playsound(audio2.wav)
 
 
 
