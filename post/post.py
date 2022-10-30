1b1dbd4435472dc984811377fba1464aa84a6043
student-ops <gogakuec@gmail.com>
2022-10-31 06:10:48 +0900

    speak for local

--- post/post.py
+++ post/post.py
@@ -3,23 +3,30 @@ import requests
 import json
 # from os import chdir
 json_data = {
-    "action": "echo"
+    # "action": "speak",
+    "speaker":1,
+    "content":"こんにちは、動的生成なのだ"
 }
 
-# url ='http://18.183.196.94:8081/handle'
+#url ='http://18.183.196.94:8081/handle'
+url ='http://localhost:8080/speak'
+# url = 'http://localhost:8082/speak'
 header = {'Content-Type':'application/json'}
-url ='http://localhost:80/handle'
-# url ='http://localhost:8081'
 d = json.dumps(json_data)
-response = requests.post(url,headers = header,data = d)
+print(d)
+response = requests.post(url,headers = header,data=d)
 # response = requests.post(url,headers = header)
-print("respose status "+str(response.status_code))
-f = open("audio3.wav","wb")
-f.write(response.content)
-f.close()
-# chdir("/Users/lakky/raspy/post")
-# playsound("audio3.wav")
-
+if response.status_code == 200:
+    print("accepted")
+    f = open("audio3.wav","wb")
+    f.write(response.content)
+    f.close()
+    # chdir("/Users/lakky/raspy/post")
+    # playsound("audio3.wav")
+else:
+    print("error respose")
+    content = json.loads(response.content)
+    print(content)
 
 
 
