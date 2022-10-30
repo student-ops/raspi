1910911fdb2685c39c0d654264b9c8aa6ee2ddfc
student-ops <gogakuec@gmail.com>
2022-10-31 07:56:23 +0900

    change json sturucture

--- post/post.py
+++ post/post.py
@@ -3,14 +3,16 @@ import requests
 import json
 # from os import chdir
 json_data = {
-    # "action": "speak",
-    "speaker":1,
-    "content":"こんにちは、動的生成なのだ"
+        "action": "speak",
+        "speak" : {
+            "speaker":1,
+            "content":"スピークで動的生成なのだ"
+        }
 }
 
-#url ='http://18.183.196.94:8081/handle'
-url ='http://localhost:8080/speak'
-# url = 'http://localhost:8082/speak'
+# url ='http://18.183.196.94:80/handle'
+url ='http://localhost:80/handle'
+# url = 'http://localhost:8080/speak'
 header = {'Content-Type':'application/json'}
 d = json.dumps(json_data)
 print(d)
@@ -24,7 +26,7 @@ if response.status_code == 200:
     # chdir("/Users/lakky/raspy/post")
     # playsound("audio3.wav")
 else:
-    print("error respose")
+    print("error respose\n")
     content = json.loads(response.content)
     print(content)
 
