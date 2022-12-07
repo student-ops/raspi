8a27c38a40a18cf10e31cef9590d556d5116dd09
student-ops <gogakuec@gmail.com>
2022-12-07 16:10:39 +0900

    a

--- test/audio_play.py
+++ test/audio_play.py
@@ -5,6 +5,7 @@ from post import json_to_audio
 from aruduino_sync import tempreture
 temp = str(tempreture.readTemp)
 text = "今の気温は" + temp +"度です"
+print(text)
 json_data = {
         "action": "speak",
         "speak" : {
