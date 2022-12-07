ceb5e611242fdd1a6be6207ed8b79a5700bf9660
student-ops <gogakuec@gmail.com>
2022-12-07 16:42:18 +0900

    confirm

--- test/audio_play.py
+++ test/audio_play.py
@@ -3,19 +3,21 @@ sys.path.append('../')
 
 from post import json_to_audio
 from aruduino_sync import tempreture
-temp = str(tempreture.readTemp)
-text = "今の気温は" + temp +"度です"
-print(text)
-json_data = {
-        "action": "speak",
-        "speak" : {
-            "speaker":1,
-            "content":text
-        }
-}
 
-print(json_to_audio.audioPost(json_data,"test"))
-# playsound("../audio/test.wav")
+if __name__ == '__main__':
+  temp = str(tempreture.readTemp())
+  text = "今の気温は" + temp +"度です"
+  print(text)
+  json_data = {
+          "action": "speak",
+          "speak" : {
+              "speaker":1,
+              "content":text
+          }
+  }
+
+  print(json_to_audio.audioPost(json_data,"test"))
+  # playsound("../audio/test.wav")
 
 
 
