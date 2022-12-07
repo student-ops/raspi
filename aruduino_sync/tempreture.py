5c5532843d340df7d398185d7179c104b158e60e
student-ops <gogakuec@gmail.com>
2022-12-07 16:43:16 +0900

    a

--- aruduino_sync/tempreture.py
+++ aruduino_sync/tempreture.py
@@ -2,7 +2,7 @@ import serial
 import time
 
 def readTemp():
-  ser = serial.Serial('/dev/ttyACMO',9600,timeout=1)
+  ser = serial.Serial('/dev/ttyACM0',9600,timeout=1)
   ser.flush()
   temp = ser.readline().decode('utf-8')
   return temp
