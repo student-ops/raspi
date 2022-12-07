a56234e760e4bf66d36df0d5a1aacac54bde5d5d
student-ops <gogakuec@gmail.com>
2022-12-07 15:30:07 +0900

    a

--- aruduino_sync/read_tempereture.py
+++ aruduino_sync/read_tempereture.py
@@ -4,7 +4,6 @@ import time
 def readTempreture():
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.flush()
-    while True:
-        ser.write(b"Hello from Raspberry Pi!\n")
-        temp = ser.readline().decode('utf-8').rstrip()
-        return temp
+    ser.write(b"Hello from Raspberry Pi!\n")
+    temp = ser.readline().decode('utf-8').rstrip()
+    return temp
