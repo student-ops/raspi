6c813d859872e2bd86bd9204b71c629b30d441f5
student-ops <gogakuec@gmail.com>
2023-04-20 16:29:41 +0900

    add read all in wi

--- tests/wi.py
+++ tests/wi.py
@@ -1,8 +1,13 @@
 import serial
+import time
 
 if __name__ == '__main__':
   ser = serial.Serial('/dev/ttyUSB0',115200,timeout =5)
   print(ser.name)
   program = b'#?\r'
   ser.write(program)
+  time.sleep(0.1)
+  result = ser.read_all()
+  print(result)
   ser.close()
+
