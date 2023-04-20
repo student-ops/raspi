5a872f010f9899d92195a51d8eed8e889f0ec0e5
student-ops <gogakuec@gmail.com>
2023-04-20 15:13:01 +0900

    a

--- test/wisun.py
+++ test/wisun.py
@@ -2,12 +2,12 @@ import sys
 sys.path.append('../')
 
 from post import json_to_audio
-from aruduino_sync import tempreture
 from playsound import playsound
 
 if __name__ == '__main__':
   temp = 12
   text = "今の気温は" + temp +"℃です"
+  url = "http://10.17.42.2/handle"
   print(text)
   json_data = {
           "action": "speak",
@@ -16,5 +16,5 @@ if __name__ == '__main__':
               "content":text
           }
   }
-  if(json_to_audio.audioPost(json_data,"temp")):
+  if(json_to_audio.audioPost(json_data,"temp",url)):
     playsound("../audio/temp.wav")
