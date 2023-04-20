2b840b194111333af989bd5fdfa2af7963e8a7f9
student-ops <gogakuec@gmail.com>
2023-04-20 18:22:21 +0900

    a

--- tests/wi.py
+++ tests/wi.py
@@ -2,12 +2,22 @@ import serial
 import time
 
 if __name__ == '__main__':
-  ser = serial.Serial('/dev/ttyUSB0',115200,timeout =5)
+
+  ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=5)
   print(ser.name)
-  program = b'#?\r'
-  ser.write(program)
+
+  # read program code from file and add \r to each line
+  with open('display.txt', 'r') as f:
+      program_lines = [line.strip() + '\r' for line in f.readlines()]
+  program = ''.join(program_lines).encode()
+
+  ser.write(b'edit 1')
   time.sleep(0.1)
-  result = ser.read_all()
+  ser.write(program)
+  ser.write(b'edit 0')
+  time.sleep(0.1)
+  ser.write(b'run')
+  time.sleep(2)
+  result = ser.readline()
   print(result)
   ser.close()
-
