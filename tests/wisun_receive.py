53bdde1d0d7fc600664e54c58170cbe9ca283049
student-ops <gogakuec@gmail.com>
2023-05-26 13:43:44 +0900

    update expression

--- tests/wisun_receive.py
+++ tests/wisun_receive.py
@@ -9,7 +9,7 @@ import sys
 
 def extract_numbers(output):
     # Extract numbers from the output using a regular expression
-    number_strings = re.findall(r"[-+]?\d*\.\d+|\d+", output)
+    number_strings = re.findall(r"[-+]?\d*\.?\d+", output)
     # Convert the strings to float
     numbers = [float(num_str) for num_str in number_strings]
     return numbers
